81346684afc03986d3e0809de63e4d1816830849
kapadiamush <kapadiamush@gmail.com>
2015-02-04 13:41:19 -0500

    implmentation file skeleton for the project

---
+++ sock352.c
@@ -0,0 +1,43 @@
+/* CS352 Socket Library Implementation */
+
+/* Inclusions */
+#include "sock352.h"
+
+/* Library Functions */
+
+/* initialzation function */
+int sock352_init(int udp_port){
+ return 0; 
+}
+
+int sock352_socket(int domain, int type, int protocol){
+  return 0; 
+}
+
+int sock352_bind(int fd, sockaddr_sock352_t *addr, socklen_t len){
+  return 0; 
+}
+
+int sock352_connect(int fd, sockaddr_sock352_t *addr, socklen_t len){
+  return 0;
+}
+
+int sock352_listen(int fd, int n){
+  return 0; 
+}
+
+int sock352_accept(int _fd, sockaddr_sock352_t *addr, int *len){
+  return 0; 
+}
+
+int sock352_close(int fd){
+  return 0; 
+}
+
+int sock352_read(int fd, void *buf, int count){
+  return 0; 
+}
+
+int sock352_write(int fd, void *buf, int count){
+  return 0; 
+}
