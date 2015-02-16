214542839b464f8de3351eb89b9083040ca42c50
Mushaheed Kapadia <kapadiamush@gmail.com>
2015-02-16 16:27:56 -0500

    added the finding socket stuff

--- sock352lib.c
+++ sock352lib.c
@@ -73,7 +73,14 @@ int sock352_socket(int domain, int type, int protocol)
  */
 int sock352_bind(int fd, sockaddr_sock352_t *addr, socklen_t len)
 {
-  return 0; 
+	socket352 *sock352; 
+
+	/*
+	 * Get the socket from the hashtable
+	 */
+	if((sock352 = findSocket(fd)) == SOCK352_FAILURE) return SOCK352_FAILURE;
+
+	return 0; 
 }
 
 /*
@@ -84,7 +91,16 @@ int sock352_bind(int fd, sockaddr_sock352_t *addr, socklen_t len)
  */ 
 int sock352_connect(int fd, sockaddr_sock352_t *addr, socklen_t len)
 {
-  return 0;
+	
+	socket352 *sock352; 
+
+	/*
+	 * Get the socket from the hashtable
+	 */
+	if((sock352 = findSocket(fd)) == SOCK352_FAILURE) return SOCK352_FAILURE;
+	
+
+	return 0;
 }
 
 /*
@@ -96,7 +112,7 @@ int sock352_connect(int fd, sockaddr_sock352_t *addr, socklen_t len)
  */
 int sock352_listen(int fd, int n)
 {
-  return 0; 
+	return 0; 
 }
 
 /*
@@ -108,7 +124,7 @@ int sock352_listen(int fd, int n)
  */
 int sock352_accept(int _fd, sockaddr_sock352_t *addr, int *len)
 {
-  return 0; 
+	return 0; 
 }
 
 /*  sock352_close
@@ -119,7 +135,7 @@ int sock352_accept(int _fd, sockaddr_sock352_t *addr, int *len)
  */
 int sock352_close(int fd)
 {
-  return 0; 
+	return 0; 
 }
 
 /*
@@ -129,10 +145,10 @@ int sock352_close(int fd)
  */
 int sock352_read(int fd, void *buf, int count)
 {
-  return 0; 
+	return 0; 
 }
 
 int sock352_write(int fd, void *buf, int count)
 {
-  return 0; 
+	return 0; 
 }
